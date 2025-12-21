92cb9e9dd27daab4c79eeca299672603b4f504a7
Adriano Ricardo Felippe Torini <adrianotorini@hotmail.com>
2025-12-21 10:54:20 -0300

    Faz separacao de bancos, 3 bancos em 1 servidor.

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "azurerm_resource_group" "rg-postech-database" {
   tags = var.tags
 }
 
-# PostgreSQL Database Module
+# PostgreSQL Database Module - Servidor Principal
 module "database" {
   source = "./modules/database"
   # Configuração obrigatória
@@ -20,8 +20,8 @@ module "database" {
   storage_mb            = var.postgresql_storage_mb
   backup_retention_days = var.postgresql_backup_retention_days
 
-  # Banco de dados
-  database_name = var.postgresql_database_name
+  # Banco de dados principal (users)
+  database_name = var.postgresql_database_name_users
 
   # Tags
   tags = merge(var.tags, {
@@ -32,29 +32,34 @@ module "database" {
   })
 }
 
-module "database-user-service" {
-  source = "./modules/database"
-  # Configuração obrigatória
-  server_name         = var.postgresql_server_name_user_service
-  location            = var.postgres_location
-  resource_group_name = azurerm_resource_group.rg-postech-database.name
+# Banco de dados adicional - Pedidos (no mesmo servidor)
+resource "azurerm_postgresql_flexible_server_database" "db_pedidos" {
+  name      = var.postgresql_database_name_pedidos
+  server_id = module.database.server_id
+  charset   = "UTF8"
+  collation = "en_US.utf8"
 
-  # Configuração econômica
-  postgresql_version    = var.postgresql_version
-  sku_name              = var.postgresql_sku_name
-  storage_mb            = var.postgresql_storage_mb
-  backup_retention_days = var.postgresql_backup_retention_days
+  depends_on = [module.database]
 
-  # Banco de dados
-  database_name = var.postgresql_database_name
+  timeouts {
+    create = "10m"
+    delete = "10m"
+  }
+}
 
-  # Tags
-  tags = merge(var.tags, {
-    Environment = var.environment
-    Project     = "FastFood-System"
-    CreatedBy   = "Terraform"
-    Module      = "PostgreSQL"
-  })
+# Banco de dados adicional - Produto (no mesmo servidor)
+resource "azurerm_postgresql_flexible_server_database" "db_produto" {
+  name      = var.postgresql_database_name_produto
+  server_id = module.database.server_id
+  charset   = "UTF8"
+  collation = "en_US.utf8"
+
+  depends_on = [module.database]
+
+  timeouts {
+    create = "10m"
+    delete = "10m"
+  }
 }
 
 module "keyvault" {
